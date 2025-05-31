34d3fb84edd9031bf7e57fabf75992cc4bfa59e1
Tony Stark <oktonystark8@gmail.com>
2025-05-30 21:58:09 -0500

    prep contd.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,23 @@
 use bevy::prelude::*;
+use bevy::window::{PresentMode, WindowMode, WindowResolution};
+
+mod keybinding;
 
 fn main() -> AppExit {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Elysium Descent".into(),
+                resolution: WindowResolution::new(1920.0, 1080.0).with_scale_factor_override(1.0),
+                resizable: true,
+                present_mode: PresentMode::AutoVsync,
+                fit_canvas_to_parent: true,
+                mode: WindowMode::Windowed,
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(keybinding::plugin)
+        .run()
 }
