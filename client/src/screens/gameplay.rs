76c2d750a731b46cae9ed48b4413228590a9fd28
Tony Stark <oktonystark8@gmail.com>
2025-06-03 20:47:30 -0500

    prep contd.

--- client/src/screens/gameplay.rs
+++ client/src/screens/gameplay.rs
@@ -3,8 +3,8 @@ use bevy::prelude::*;
 use super::{Screen, despawn_scene};
 
 pub fn plugin(app: &mut App) {
-    app.add_systems(OnEnter(Screen::MainMenu), spawn)
-        .add_systems(OnExit(Screen::MainMenu), despawn_scene::<GameplayScene>);
+    app.add_systems(OnEnter(Screen::GamePlay), spawn)
+        .add_systems(OnExit(Screen::GamePlay), despawn_scene::<GameplayScene>);
 }
 
 #[derive(Component)]
